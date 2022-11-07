eea968786cdbb7ee8a134c6b719abc79c2a6afea
unknown <cornejo.n0103@gmail.com>
2022-11-07 08:08:21 -0500

    commit inicial

---
+++ src/components/component_a/component-a/component-a.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentAComponent } from './component-a.component';
+
+
+
+@NgModule({
+  declarations: [ComponentAComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentAModule { }
